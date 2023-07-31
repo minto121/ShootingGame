5b2cf16e78323be3615c1e801334c2ee1b3af740
minto121 <tomohide-matsuda-k22h@stu.kbc.ac.jp>
2023-07-31 13:13:48 +0900

    a

--- GameMainScene.cpp
+++ GameMainScene.cpp
@@ -1,2 +1,1 @@
-
 #include "GameMainScene.h"
--- GameMainScene.h
+++ GameMainScene.h
@@ -1,5 +1,18 @@
 #pragma once
-class GameMainScene
+#include"AbstractScene.h"
+class GameMainScene:public AbstractScene
 {
+
+	//コンストラクタ
+	GameMainScene();
+
+	//デストラクタ
+	~GameMainScene() {};
+
+	//描画以外の更新を実行
+	AbstractScene* Update() = 0;
+
+	//描画に関することを実装
+	void Draw() const = 0;
 };
 
